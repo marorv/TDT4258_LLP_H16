e989863a60d985f0826af38656770c0807cb2805
Maren Orvedal <marenor@stud.ntnu.no>
2017-11-20 14:04:28 +0100

    Remove unneeded stuff.

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/maketestdriver.sh
+++
@@ -1,6 +0,0 @@
-ptxdist clean test-driver-gamepad
-ptxdist compile test-driver-gamepad
-ptxdist targetinstall test-driver-gamepad
-ptxdist targetinstall kernel
-ptxdist image root.romfs
-ptxdist test flash-rootfs
